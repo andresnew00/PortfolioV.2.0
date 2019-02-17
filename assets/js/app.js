3fb9e528bb8c7fcb19aba4095c3576c4b0318d1c
andresnew00 <andresnew00@hotmail.com>
2019-02-17 12:40:42 -0500

    added a new video, switch functionality, better design, navbar working properly, and about me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,18 @@
-// $(function() {
-                
-//     var welcomeSection = $('.welcome-section'),
-//         enterButton = welcomeSection.find('.enter-button');
-    
-        
-//     setTimeout(function() {
-//         welcomeSection.removeClass('content-hidden');
-//     }, 500);
-    
-//     enterButton.on('click', function(e) {
-//         e.preventDefault();
-//         welcomeSection.addClass('content-hidden').fadeOut();
-//     });
-    
-    
-// });
-//     // layout
-
-//     (function (window, document) {
-
-//         var layout   = document.getElementById('layout'),
-//             menu     = document.getElementById('menu'),
-//             menuLink = document.getElementById('menuLink'),
-//             content  = document.getElementById('main');
-    
-//         function toggleClass(element, className) {
-//             var classes = element.className.split(/\s+/),
-//                 length = classes.length,
-//                 i = 0;
-    
-//             for(; i < length; i++) {
-//               if (classes[i] === className) {
-//                 classes.splice(i, 1);
-//                 break;
-//               }
-//             }
-//             // The className is not found
-//             if (length === classes.length) {
-//                 classes.push(className);
-//             }
-    
-//             element.className = classes.join(' ');
-//         }
-    
-//         function toggleAll(e) {
-//             var active = 'active';
-    
-//             e.preventDefault();
-//             toggleClass(layout, active);
-//             toggleClass(menu, active);
-//             toggleClass(menuLink, active);
-//         }
-    
-//         menuLink.onclick = function (e) {
-//             toggleAll(e);
-//             console.log("hello");
-//         };
-    
-//         content.onclick = function(e) {
-//             if (menu.className.indexOf('active') !== -1) {
-//                 toggleAll(e);
-//             }
-//         };
-    
-//     }(this, this.document));
 
     $('#check').on("click", function(){
         if($(this).is(":checked")) {
-            console.log("Clicked slider!")
             $('.full-body').addClass('light');
+            $('.navbar').removeClass('bg-dark');
+            $('.navbar').addClass('nav-light');
+            $('#about').addClass('about-light');
+            $('#contact-box').addClass('about-light');
+            $('#portfolio-class').addClass('about-light');
         } else {
           $('.full-body').removeClass('light');
+          $('.navbar').addClass('bg-dark');
+          $('.navbar').removeClass('nav-light');
+          $('#about').removeClass('about-light');
+          $('#contact-box').removeClass('about-light');
+          $('#portfolio-class').removeClass('about-light');
         }
     });
